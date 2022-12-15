46fb6f121dc144c32b68356278e23a9f9dc81eb7
Ray Zana <rayzana280@gmail.com>
2022-12-15 11:36:57 -0500

    commit tree 8a2f8501bbb64974354eacade578cedccb930b87

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 =2 
+let multiply = num1 * num2
 
+const random = Math.ceil(Math.random()*10) +1
+
+const num3= 10
+const num4 = 6
+let mod = num3 % num4
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 // Write your code here
